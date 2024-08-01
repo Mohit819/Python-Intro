e62b29657c7f12a351526704095937e27c73e3d6
Mohit819 <immohitrana06@gmail.com>
2024-08-01 19:03:26 -0400

    Add files via upload

---
+++ Intro to Python/1.cw.py
@@ -0,0 +1,30 @@
+import turtle
+
+bob = turtle.Turtle()
+
+#Square
+##bob.forward(100)
+##bob.right(90)
+##bob.forward(100)
+##bob.right(90)
+##bob.forward(100)
+##bob.right(90)
+##bob.forward(100)
+##bob.right(90)
+
+#Triangle
+##bob.forward(100)
+##bob.left(120)
+##bob.forward(100)
+##bob.left(120)
+##bob.forward(100)
+##bob.left(120)
+
+bob.backward(100)
+bob.left(90)
+bob.backward(100)
+bob.left(90)
+bob.backward(100)
+bob.left(90)
+bob.backward(100)
+bob.left(90)
