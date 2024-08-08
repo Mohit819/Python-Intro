22cdf20d5565d2ebc618d4691edfbafa0e59d40f
Mohit819 <immohitrana06@gmail.com>
2024-08-08 19:05:22 -0400

    Add files via upload

---
+++ Intro to Python/challenge file.py
@@ -0,0 +1,12 @@
+import turtle
+##BMW
+JohnCena = turtle.Turtle()
+JohnCena.pencolor('black')
+JohnCena.fillcolor('black')
+JohnCena.begin_fill()
+JohnCena.circle(150)
+JohnCena.end_fill()
+JohnCena.goto(0,50)
+JohnCena.pencolor('white')
+JohnCena.fillcolor('white')
+JohnCena.begin_fill
