ba70bc5da1ecd26f0654a30c5bf57354b9309a03
Mohit819 <immohitrana06@gmail.com>
2024-08-20 18:02:06 -0400

    Add files via upload

---
+++ Intro to Python/6.cw.py
@@ -0,0 +1,66 @@
+import turtle
+##kayla = turtle.Turtle()
+##obie = turtle.Turtle()
+##john = turtle.Turtle()
+##holly = turtle.Turtle() # we made 4 different turtles!
+##kayla.color("red")
+##obie.color("green")
+##john.color("purple")
+##holly.color("orange") # they all change colour one by one
+##kayla.speed(1)
+##obie.speed(0)
+##john.speed(4) # who is the fastest?
+##kayla.goto(0,100) # remember week 4 - all about the grid!
+##obie.goto(100, 0) # where will they end up?
+##john.goto(-100, 0)
+##holly.goto(0, -100)
+##kayla.hideturtle() # kayla will disappear!
+scren = turtle.Screen() # this is like creating a turtle
+################################################################################################################################################################################################################################################################################################################################################################################################################################################################# but this time we are creating a Screen
+################################################################################################################################################################################################################################################################################################################################################################################################################################################################screen.bgcolor("blue") # set the colour
+################################################################################################################################################################################################################################################################################################################################################################################################################################################################screen.setup(300, 300) # set the size
+##scren.bgcolor('purple')
+##scren.setup(1000,1000)
+import turtle
+##sasha = turtle.Turtle() # creating a turtle
+##screen = turtle.Screen() # creating a screen
+##screen.bgcolor("yellow") # make the screen yellow
+##sasha.forward(100) # sasha draws a triangle
+##sasha.right(120)
+##sasha.forward(100)
+##sasha.right(120)
+##sasha.forward(100)
+##screen.reset() # the triangle is gone!
+##sasha.forward(100)
+##screen.clear() # everything is gone!
+jimah = turtle.Screen()
+jimah.bgcolor('light blue')
+jimah.setup(800,600)
+tod = turtle.Turtle()
+tod.penup()
+tod.goto(0,-350)
+tod.pendown()
+tod.color('red')
+tod.begin_fill()
+tod.circle(280)
+tod.end_fill()
+tod.color('orange')
+tod.begin_fill()
+tod.circle(260)
+tod.end_fill()
+tod.color('yellow')
+tod.begin_fill()
+tod.circle(240)
+tod.end_fill()
+tod.color('green')
+tod.begin_fill()
+tod.circle(220)
+tod.end_fill()
+tod.color('blue')
+tod.begin_fill()
+tod.circle(200)
+tod.end_fill()
+tod.color('purple')
+tod.begin_fill()
+tod.circle(180)
+tod.end_fill()
